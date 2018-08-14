3c7fef6282355da893fca6fe22cff5ae131e24b2
James Connolly <james.connolly@lyit.ie>
2018-08-14 14:30:33 +0100

    Added combobox to app

--- WPFHelloWorld/WPFDemoProject/MainWindow.xaml.cs
+++ WPFHelloWorld/WPFDemoProject/MainWindow.xaml.cs
@@ -20,15 +20,38 @@ namespace WPFDemoProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Student> students = new List<Student>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            students.Add(new Student { FirstName = "James", LastName = "Connolly" });
+            students.Add(new Student { FirstName = "Joe", LastName = "Smith" });
+            students.Add(new Student { FirstName = "Jane", LastName = "Jones" });
+            students.Add(new Student { FirstName = "Fred", LastName = "Soap" });
+
+            cboStudentNames.ItemsSource = students;
         }
 
         private void btnClickMe_Click(object sender, RoutedEventArgs e)
         {
-            String firstName = tbxFirstName.Text.Trim();
+            string firstName = tbxFirstName.Text.Trim();
             MessageBox.Show($"Your first name is {firstName}");
+        }
+
+        public class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string FullName
+            {
+                get
+                {
+                    return $"{ FirstName} { LastName }";
+                }
+            }
         }
     }
 }
